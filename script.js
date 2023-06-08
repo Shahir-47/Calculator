17482fe74f7efc36da8589548cd7402d7ef0098f
Shahir Ahmed <ahmeds@dickinson.edu>
2023-06-08 12:45:18 -0400

    create some basic functions and html layout

---
+++ script.js
@@ -0,0 +1,36 @@
+let a = 0;
+let b = 0;
+let operator = '';
+
+const add = function (a, b) {
+    return a + b;
+  };
+  
+const subtract = function (a, b) {
+    return a - b;
+};
+
+const multiply = function (a, b) {
+    return a * b;
+}
+
+const divide = function (a, b) {   
+    return a / b;
+}
+
+const operate = function (operator, a, b) {
+    if (operator === '+') {
+        return add(a, b);
+    } else if (operator === '-') {
+        return subtract(a, b);
+    } else if (operator === '*') {
+        return multiply(a, b);
+    } else if (operator === '/') {
+        return divide(a, b);
+    }
+}
+
+function updateDisplay() {
+    let display = document.querySelector('#display');
+    display.textContent = a;
+}
