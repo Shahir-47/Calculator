142ba247013c3594307f16b6d871bf0ffd765081
Shahir Ahmed <ahmeds@dickinson.edu>
2023-06-08 14:11:17 -0400

    got some calculations right

--- script.js
+++ script.js
@@ -31,6 +31,83 @@ const operate = function (operator, a, b) {
 }
 
 function updateDisplay() {
-    let display = document.querySelector('#display');
-    display.textContent = a;
+    let display = document.querySelector('.display__result');
+    display.color = 'black';
+    display.textContent = parseInt(a);
+    if (operator != '') {
+        display.textContent = parseInt(b);
+    }
 }
+
+function historyDisplay() {
+    let display = document.querySelector('.display__history');
+    display.color = 'black';
+    display.textContent = a;
+    display.textContent += operator;
+}
+
+// function clearDisplay() {
+//     let display = document.querySelector('.display__result');
+//     display.textContent = '';
+//     display = document.querySelector('.display__history');
+//     display.textContent = '';
+// }
+
+// const equalsButton = document.querySelector('.buttons__item--equals');
+// equalsButton.addEventListener('click', () => {
+//     a = operate(operator, a, b);
+//     updateDisplay();
+//     operator = '';
+//     b = 0;
+// });
+
+// const clearButton = document.querySelector('.buttons__item--clear');
+// clearButton.addEventListener('click', () => {
+//     clearDisplay();
+//     a = 0;
+//     b = 0;
+//     operator = '';
+// });
+
+// function deleteLast() {
+//     let display = document.querySelector('.display__result');
+//     display.textContent = display.textContent.slice(0, -1);
+// }
+
+// const deleteButton = document.querySelector('.buttons__item--delete');
+// deleteButton.addEventListener('click', () => {
+//     deleteLast();
+// });
+
+function clearResult() {
+    let display = document.querySelector('.display__result');
+    display.textContent = '';
+}
+
+const numberButtons = document.querySelectorAll('.buttons__item--number');
+numberButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        if (operator === '') {
+            a = parseInt(a + button.textContent);
+        }
+        else {
+            b = parseInt(b + button.textContent);
+        }
+        console.log(a + " " + operator + " " + b);
+        updateDisplay();
+    });
+});
+
+const operatorButtons = document.querySelectorAll('.buttons__item--operator');
+operatorButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        operator = button.textContent;
+        if (a != 0 && b!=0) {
+            a = operate(operator, a, b);
+            updateDisplay();
+            operator = '';    
+            b = 0;
+        }
+        historyDisplay();
+    });
+});
